7bbe546fd45ea1842c5b055c5787555543e0f851
Eduardo Riol <eduriol@gmail.com>
2016-05-25 14:07:01 +0000

    post buttons grayed

--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -44,22 +44,20 @@ export default class Post extends Component {
           { !this.state.edit ?
             <div>
               { (Meteor.userId() === this.props.post.ownerId) ?
-                <div className="postInputs">
-                  <div className="checkbox">
-                    <label>
-                      <input
-                        type="checkbox"
-                        readOnly
-                        checked={this.props.post.isPublic}
-                        onClick={this.toggleIsPublic.bind(this)}/> make public
-                    </label>
-                  </div>
-                  <button type="button" className="btn btn-link" onClick={this.changeToEditMode.bind(this)}>
+                <div className="pull-right">
+                  <button type="button" className="btn btn-link gray" onClick={this.changeToEditMode.bind(this)}>
                     <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                   </button>
-                  <button type="button" className="btn btn-link" onClick={this.deleteThisPost.bind(this)}>
+                  <button type="button" className="btn btn-link gray" onClick={this.deleteThisPost.bind(this)}>
                     <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
                   </button>
+                  <label className="checkbox-inline">
+                    <input
+                      type="checkbox"
+                      readOnly
+                      checked={this.props.post.isPublic}
+                      onClick={this.toggleIsPublic.bind(this)}/><small>make public</small>
+                  </label>
                 </div> : ''
               }
               <h2>
